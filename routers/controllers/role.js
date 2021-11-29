04e45cafe12806c4bc162ea3234ecadb4500d739
ShathaAlmansour <>
2021-11-29 20:38:37 +0300

    added

--- routers/controllers/role.js
+++ routers/controllers/role.js
@@ -1,30 +1,29 @@
 const roleModel = require("./../../db/models/role");
 
-const createRole =
-(req,res) => {
-    const {role , permissions} = req.body
-    const newRole = new roleModel ({
-        role,
-        permissions,
-    });
-    newRole
+const createRole = (req, res) => {
+  const newRole = new roleModel(req.body);
+  newRole
     .save()
     .then((result) => {
-        res.status(201).json(result);
+      res.status(201).json(result);
     })
     .catch((err) => {
-        res.status(400).json(result)
+      res.status(400).json(err);
     });
-}
+};
 
-const allRoles = (req, res) => {
-    roleModel
+const roles = (req, res) => {
+  roleModel
     .find({})
-    .then ((result) => {
-        res.status(200).json(result);
+    .then((result) => {
+      res.status(200).json(result);
     })
     .catch((err) => {
-        res.status(400).json(err);
-    })
-}
-module.exports = {createRole,allRoles};
+      res.status(400).json(err);
+    });
+};
+
+module.exports = {
+  createRole,
+  roles,
+};
