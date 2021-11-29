fe7f31228240e4c4e4ad53a9ce1ebc008c2b2b4b
ShathaAlmansour <>
2021-11-29 20:47:07 +0300

    added

--- routers/routes/user.js
+++ routers/routes/user.js
@@ -2,3 +2,8 @@ const express = require('express')
 const  {register ,login} = require('./../controllers/user')
 const userRouter = express.Router()
 
+userRouter.post("/register", register);
+userRouter.post("/login", login);
+
+
+module.exports = userRouter;
